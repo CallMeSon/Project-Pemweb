2848cb451a9e6d9147949e764ca28cc64e50e8b9
Gerson Sebastian <sebastiangerson24@gmail.com>
2025-11-21 10:28:53 +0700

    visual error fix

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ $result_categories = $conn->query("SELECT id, nama_kategori FROM categories ORDE
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Selamat Datang di Kedai Kopi</title>
+    <title>Selamat Datang di Radal&Beans</title>
     <link rel="stylesheet" href="css/index.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 </head>
